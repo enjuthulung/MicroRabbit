ec182484776fd1255832cd431324542ffcd76276
enju.rai <enju.rai@nestrait.com>
2019-09-09 15:54:36 +0545

    Microservice

--- MicroRabbit.Banking.Application/Services/AccountService.cs
+++ MicroRabbit.Banking.Application/Services/AccountService.cs
@@ -1,4 +1,7 @@
-﻿using MicroRabbit.Banking.Application.Interfaces;
+﻿using MicorRabbit.Domain.Core.Bus;
+using MicroRabbit.Banking.Application.Interfaces;
+using MicroRabbit.Banking.Application.Models;
+using MicroRabbit.Banking.Domain.Commands;
 using MicroRabbit.Banking.Domain.Interfaces;
 using MicroRabbit.Banking.Domain.Models;
 using System;
@@ -10,15 +13,28 @@ namespace MicroRabbit.Banking.Application.Services
     public class AccountService : IAccountService
     {
         private readonly IAccountRepository _accountRepository;
+        private readonly IEventBus _bus;
 
-        public AccountService(IAccountRepository accountRepository)
+        public AccountService(IAccountRepository accountRepository, IEventBus eventBus)
         {
             _accountRepository = accountRepository;
+            _bus = eventBus;
         }
 
         public IEnumerable<Account> GetAccounts()
         {
             return _accountRepository.GetAccounts();
+        }
+
+        public void Transfer(AccountTransfer accountTransfer)
+        {
+            var createTrandferCommand = new CreateTransferCommand(
+                accountTransfer.FromAccount,
+                accountTransfer.ToAccount,
+                accountTransfer.TransferAmount
+                );
+
+            _bus.SendCommand(createTrandferCommand);
         }
     }
 }
--- MicroRabbit.Infra.IoC/DependencyContainer.cs
+++ MicroRabbit.Infra.IoC/DependencyContainer.cs
@@ -1,8 +1,11 @@
-﻿using MicorRabbit.Domain.Core.Bus;
+﻿using MediatR;
+using MicorRabbit.Domain.Core.Bus;
 using MicroRabbit.Banking.Application.Interfaces;
 using MicroRabbit.Banking.Application.Services;
 using MicroRabbit.Banking.Data.Context;
 using MicroRabbit.Banking.Data.Repository;
+using MicroRabbit.Banking.Domain.CommandHandlers;
+using MicroRabbit.Banking.Domain.Commands;
 using MicroRabbit.Banking.Domain.Interfaces;
 using MicroRabbit.Infra.Bus;
 using Microsoft.Extensions.DependencyInjection;
@@ -18,6 +21,9 @@ namespace MicroRabbit.Infra.IoC
         {
             //Domain Bus
             services.AddTransient<IEventBus, RabbitMQBus>();
+
+            //Domain Banking Commands
+            services.AddTransient<IRequestHandler<CreateTransferCommand, bool>, TransferCommandHandler>();
 
             //Application Services
             services.AddTransient<IAccountService, AccountService>();
