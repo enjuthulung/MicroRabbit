547380ce0e73e1fd0fa784288c637495edf67db6
enju.rai <enju.rai@nestrait.com>
2019-09-09 15:05:20 +0545

    Partially completed Banking Microservice

--- MicroRabbit.Infra.IoC/DependencyContainer.cs
+++ MicroRabbit.Infra.IoC/DependencyContainer.cs
@@ -1,4 +1,9 @@
 ﻿using MicorRabbit.Domain.Core.Bus;
+using MicroRabbit.Banking.Application.Interfaces;
+using MicroRabbit.Banking.Application.Services;
+using MicroRabbit.Banking.Data.Context;
+using MicroRabbit.Banking.Data.Repository;
+using MicroRabbit.Banking.Domain.Interfaces;
 using MicroRabbit.Infra.Bus;
 using Microsoft.Extensions.DependencyInjection;
 using System;
@@ -13,6 +18,14 @@ namespace MicroRabbit.Infra.IoC
         {
             //Domain Bus
             services.AddTransient<IEventBus, RabbitMQBus>();
+
+            //Application Services
+            services.AddTransient<IAccountService, AccountService>();
+
+            //Data
+            services.AddTransient<IAccountRepository, AccounyRepository>();
+            services.AddTransient<BankingDbContext>();
+
         }
     }
 }
